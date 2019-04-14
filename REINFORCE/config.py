3a6867e4ee83e35a1b5e3b95b8e09f4cf9ef3510
jethack <jethack23@gmail.com>
2019-04-15 04:01:59 +0900

    REINFORCE

---
+++ REINFORCE/config.py
@@ -0,0 +1,12 @@
+class Config:
+    env_name = "CartPole-v0"
+
+    input_shape = (4,)
+
+    fc_archi = [16, 32]
+
+    max_ep = 2000
+
+    lr = 0.0001
+
+    df = 0.99
---
+++ REINFORCE/environment.py
@@ -0,0 +1,39 @@
+import gym
+
+import numpy as np
+
+
+class Environment:
+
+    def __init__(self, config):
+        self.config = config
+        self.make()
+
+    def initialize_game(self):
+        self._state = self.env.reset()
+        return self.state
+
+    def _step(self, action):
+        self._state, self.reward, self.done, self.info = self.env.step(action)
+
+    def render(self):
+        self.env.render()
+
+    @property
+    def action_space_size(self):
+        return self.env.action_space.n
+
+    @property
+    def state(self):
+        return self._state
+
+    def act(self, action):
+        self._step(action)
+        return self.state, self.reward, self.done
+
+    def make(self):
+        self.env = gym.make(self.config.env_name)
+        self._state = None
+        self.reward = 0
+        self.done = True
+        self.info = None
